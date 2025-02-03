d80f3bfb56f69525b32d085a3fa6764be85112ef
isifire@outlook.com <isifire@outlook.com>
2025-02-03 19:48:09 +0100

    hecho

--- app/src/main/java/com/uomovil/tanteo/MainActivity.kt
+++ app/src/main/java/com/uomovil/tanteo/MainActivity.kt
@@ -1,48 +1,96 @@
 package com.uomovil.tanteo
 
 import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
 import androidx.appcompat.app.AppCompatActivity
 import com.uomovil.tanteo.databinding.ActivityMainBinding
 
-// Data class para almacenar los puntos de los equipos
-data class Team(var puntosLocal: Int = 0, var puntosVisitante: Int = 0, var d3: Int = 0)
+// Clase parcelable para almacenar las anotaciones de cada equipo
+data class Team(
+    var anotaciones1: Int = 0, // Cantidad de veces que anotaron 1 punto
+    var anotaciones2: Int = 0, // Cantidad de veces que anotaron 2 puntos
+    var anotaciones3: Int = 0  // Cantidad de veces que anotaron 3 puntos
+) : Parcelable {
+
+    constructor(parcel: Parcel) : this(
+        parcel.readInt(),
+        parcel.readInt(),
+        parcel.readInt()
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeInt(anotaciones1)
+        parcel.writeInt(anotaciones2)
+        parcel.writeInt(anotaciones3)
+    }
+
+    override fun describeContents(): Int = 0
+
+    // Calcular el total sumando (cantidad * valor de cada anotación)
+    fun calcularTotal(): Int = (anotaciones1 * 1) + (anotaciones2 * 2) + (anotaciones3 * 3)
+
+    companion object CREATOR : Parcelable.Creator<Team> {
+        override fun createFromParcel(parcel: Parcel): Team = Team(parcel)
+        override fun newArray(size: Int): Array<Team?> = arrayOfNulls(size)
+    }
+}
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding // ViewBinding
-    private var equipo = Team() // Instancia para manejar los puntos
+    private lateinit var binding: ActivityMainBinding
+    private var equipoLocal = Team()
+    private var equipoVisitante = Team()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root) // Se usa binding en lugar de setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
-        // Inicializar los TextView con el valor inicial de los puntos
-        binding.textLocalPuntos.text = equipo.puntosLocal.toString()
-        binding.textVisitantePuntos.text = equipo.puntosVisitante.toString()
+        // Restaurar estado si existe
+        equipoLocal = savedInstanceState?.getParcelable("equipoLocal") ?: Team()
+        equipoVisitante = savedInstanceState?.getParcelable("equipoVisitante") ?: Team()
 
-        // Botón para sumar 1 punto al equipo local
+        // Actualizar la UI con los valores actuales
+        actualizarMarcador()
+
+        // Botones para sumar puntos al equipo local
         binding.buttonPlus1Local.setOnClickListener {
-            equipo.puntosLocal++
-            binding.textLocalPuntos.text = equipo.puntosLocal.toString()
+            equipoLocal.anotaciones1++
+            actualizarMarcador()
         }
-
-        // Botón para sumar 2 puntos al equipo local
         binding.buttonPlus2Local.setOnClickListener {
-            equipo.puntosLocal += 2
-            binding.textLocalPuntos.text = equipo.puntosLocal.toString()
+            equipoLocal.anotaciones2++
+            actualizarMarcador()
+        }
+        binding.buttonPlus3Local.setOnClickListener {
+            equipoLocal.anotaciones3++
+            actualizarMarcador()
         }
 
-        // Botón para sumar 1 punto al equipo visitante
+        // Botones para sumar puntos al equipo visitante
         binding.buttonPlus1Visitante.setOnClickListener {
-            equipo.puntosVisitante++
-            binding.textVisitantePuntos.text = equipo.puntosVisitante.toString()
+            equipoVisitante.anotaciones1++
+            actualizarMarcador()
         }
-
-        // Botón para sumar 2 puntos al equipo visitante
         binding.buttonPlus2Visitante.setOnClickListener {
-            equipo.puntosVisitante += 2
-            binding.textVisitantePuntos.text = equipo.puntosVisitante.toString()
+            equipoVisitante.anotaciones2++
+            actualizarMarcador()
         }
+        binding.buttonPlus3Visitante.setOnClickListener {
+            equipoVisitante.anotaciones3++
+            actualizarMarcador()
+        }
+    }
+
+    private fun actualizarMarcador() {
+        binding.textLocalPuntos.text = equipoLocal.calcularTotal().toString()
+        binding.textVisitantePuntos.text = equipoVisitante.calcularTotal().toString()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putParcelable("equipoLocal", equipoLocal)
+        outState.putParcelable("equipoVisitante", equipoVisitante)
     }
 }
